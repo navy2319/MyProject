b33e01165bf3ff8fb76441045d61b7d5c3c77018
navy2319 <nscheffert@madisoncollege.edu>
2016-05-12 11:37:22 -0500

    adding javadoc

--- src/farm/entities/Members.java
+++ src/farm/entities/Members.java
@@ -3,6 +3,8 @@ package farm.entities;
 /**
  * Created by scheffs on 3/10/2016.
  */
+
+
 public class Members {
 
     private int memberId;
@@ -11,42 +13,82 @@ public class Members {
     private String email;
     private String message;
 
+    /**
+     * gets memberId
+     * @return memberId
+     */
     public int getMemberId() {
         return memberId;
     }
 
+    /**
+     * sets memberId
+     * @param memberId new memberId
+     */
     public void setMemberId(int memberId) {
         this.memberId = memberId;
     }
 
+    /**
+     * gets firstName
+     * @return firstName
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * sets firstName
+     * @param firstName new firstName
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * gets lastName
+     * @return lastName
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * sets lastName
+     * @param lastName new lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * gets email
+     * @return email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * sets email
+     * @param email new email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * gets message
+     * @return message
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * sets message
+     * @param message new message
+     */
     public void setMessage(String message) {
         this.message = message;
     }
--- src/farm/entities/users.java
+++ src/farm/entities/users.java
@@ -13,9 +13,20 @@ public class Users {
     private String firstName;
     private String lastName;
 
+    /**
+     * Default constructor
+     */
     public Users() {
     }
 
+    /**
+     * Constructor with params
+     * @param userId userId
+     * @param userName userName
+     * @param userPassword userPassword
+     * @param lastName lastName
+     * @param firstName firstName
+     */
     public Users(int userId, String userName, String userPassword, String lastName, String firstName) {
         this.userId = userId;
         this.userName = userName;
@@ -24,43 +35,82 @@ public class Users {
         this.firstName = firstName;
     }
 
-    /**/
+    /**
+     * Gets userID
+     * @return userID
+     */
     public int getUserId() {
         return userId;
     }
 
+    /**
+     * Sets userId
+     * @param userId new value of userId
+     */
     public void setUserId(int userId) {
         this.userId = userId;
     }
 
+    /**
+     * gets userName
+     * @return userName
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * sets userName
+     * @param userName new value of userName
+     */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    /**
+     * gets userPassword
+     * @return userPassword
+     */
     public String getUserPassword() {
         return userPassword;
     }
 
+    /**
+     * sets userPassword
+     * @param userPassword new userPassword
+     */
     public void setUserPassword(String userPassword) {
         this.userPassword = userPassword;
     }
 
+    /**
+     * gets lastName
+     * @return lastName
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * sets lastName
+     * @param lastName new lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * gets firstName
+     * @return firstName
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * sets firstName
+     * @param firstName new firstName
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
